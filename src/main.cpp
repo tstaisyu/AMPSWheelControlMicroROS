22ee3bac93f044ae038d56a7c0f0e4ad987db35f
taisyu shibata <t_shiba117@yahoo.co.jp>
2024-09-05 11:37:36 +0900

    Add setupM5stack function

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,5 @@
-#include <Arduino.h>
+#include <M5Stack.h>
+#include "SetupM5stack.h"
 
 // put function declarations here:
 int myFunction(int, int);
@@ -6,6 +7,8 @@ int myFunction(int, int);
 void setup() {
   // put your setup code here, to run once:
   int result = myFunction(2, 3);
+
+  setupM5stack();  
 }
 
 void loop() {
