bc3d904102797bd9c3aa3179d95bd7fb44484db3
taisyu shibata <t_shiba117@yahoo.co.jp>
2024-09-06 00:24:11 +0900

    Delete unused code

--- src/main.cpp
+++ src/main.cpp
@@ -1,21 +1,13 @@
 #include <M5Stack.h>
 #include "SetupM5stack.h"
 
-// put function declarations here:
-int myFunction(int, int);
-
 void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
 
   setupM5stack();  
+
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+
 }
 
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
-}
