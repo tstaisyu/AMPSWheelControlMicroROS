5883fd5ff85882a1ad0ec67d9832b9547ac0cba4
taisyu shibata <t_shiba117@yahoo.co.jp>
2024-10-17 05:46:43 +0900

    Create IMUManager class and add calibrateSensors and applyLowPassFilter functions

--- src/IMUManager.cpp
+++ src/IMUManager.cpp
@@ -13,3 +13,75 @@
  * limitations under the License.
  */
 
+#include "IMUManager.h"
+
+IMUManager::IMUManager() : lpf_beta(0.1), accX_filtered(0.0), gyroX_filtered(0.0) {
+    // 初期化処理
+}
+
+void IMUManager::initialize() {
+    M5.IMU.Init();
+    calibrateSensors();
+}
+
+bool IMUManager::update() {
+    // IMUからデータを読み取る
+    M5.IMU.getAccelData(&ax, &ay, &az);
+    M5.IMU.getGyroData(&gx, &gy, &gz);
+
+    // オフセット適用
+    ax -= accOffset[0];
+    ay -= accOffset[1];
+    az -= accOffset[2]; // 重力加速度を考慮
+
+    gx -= gyroOffset[0];
+    gy -= gyroOffset[1];
+    gz -= gyroOffset[2];
+
+    applyLowPassFilter();
+
+    return true;
+}
+
+void IMUManager::applyLowPassFilter() {
+    // 加速度とジャイロスコープのデータをフィルタリング
+    accX_filtered = accX_filtered + lpf_beta * (ax - accX_filtered);
+    accY_filtered = accY_filtered + lpf_beta * (ay - accY_filtered);
+    accZ_filtered = accZ_filtered + lpf_beta * (az - accZ_filtered);    
+    gyroX_filtered = gyroX_filtered + lpf_beta * (gx - gyroX_filtered);
+    gyroY_filtered = gyroY_filtered + lpf_beta * (gy - gyroY_filtered);
+    gyroZ_filtered = gyroZ_filtered + lpf_beta * (gz - gyroZ_filtered);
+}
+
+void IMUManager::getCalibratedData(float &aX, float &aY, float &aZ, float &gX, float &gY, float &gZ) {
+    aX = accX_filtered;
+    aY = accY_filtered;
+    aZ = accZ_filtered;
+    gX = gyroX_filtered;
+    gY = gyroY_filtered;
+    gZ = gyroZ_filtered;
+}
+
+void IMUManager::calibrateSensors() {
+    float sumAx = 0, sumAy = 0, sumAz = 0;
+    float sumGx = 0, sumGy = 0, sumGz = 0;
+    const int samples = 500;
+    for (int i = 0; i < samples; i++) {
+        M5.IMU.getAccelData(&ax, &ay, &az);
+        M5.IMU.getGyroData(&gx, &gy, &gz);
+        sumAx += ax;
+        sumAy += ay;
+        sumAz += az;
+        sumGx += gx;
+        sumGy += gy;
+        sumGz += gz;
+        delay(2);
+    }
+
+    accOffset[0] = sumAx / samples;
+    accOffset[1] = sumAy / samples;
+    accOffset[2] = (sumAz / samples) - 1.0; // M5スティックが水平であると仮定
+    gyroOffset[0] = sumGx / samples;
+    gyroOffset[1] = sumGy / samples;
+    gyroOffset[2] = sumGz / samples;
+}
