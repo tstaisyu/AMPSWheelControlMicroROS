081a14983f54fec0baede22d6653e150f89cbc6c
taisyu shibata <t_shiba117@yahoo.co.jp>
2024-10-20 02:27:55 +0900

    Add variables related to ahrs

--- include/IMUManager.h
+++ include/IMUManager.h
@@ -23,16 +23,16 @@ public:
     IMUManager();
     void initialize();
     bool update();
-    void getCalibratedData(float &ax, float &ay, float &az, float &gx, float &gy, float &gz, float &mX, float &mY, float &mZ);
+    void getCalibratedData(float &ax, float &ay, float &az, float &gx, float &gy, float &gz, float &mx, float &my, float &mz);
 
 private:
     float ax, ay, az; // 加速度センサのデータ
     float gx, gy, gz; // ジャイロセンサのデータ
     float mx, my, mz;  // 地磁気データ
-    float accOffset[3], gyroOffset[3]; // キャリブレーションオフセット
+    float accOffset[3], gyroOffset[3], ahrsOffset[3]; // キャリブレーションオフセット
 
     float lpf_beta; // フィルタの係数
-    float accX_filtered, accY_filtered, accZ_filtered, gyroX_filtered, gyroY_filtered, gyroZ_filtered; // フィルタリングされたデータ
+    float accX_filtered, accY_filtered, accZ_filtered, gyroX_filtered, gyroY_filtered, gyroZ_filtered, ahrsX_filtered, ahrsY_filtered, ahrsZ_filtered; // フィルタリングされたデータ
 
     void calibrateSensors(); // センサのキャリブレーションを行う
     void applyLowPassFilter(); // ローパスフィルタを適用する
